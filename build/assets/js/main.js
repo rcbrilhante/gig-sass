fc90d699bc410778f5df9cddfc5c408f81b5e3e4
rcbrilhante <r.brilhante@sapo.pt>
2019-07-01 10:39:34 +0100

    Sass gig

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,1 +1,9 @@
-document.addEventListener("DOMContentLoaded",function(){});
+// ------------------------------------------------------------------
+// SCRIPTS
+// ------------------------------------------------------------------
+
+document.addEventListener("DOMContentLoaded", function() {
+  // ...
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVxyXG4vLyBTQ1JJUFRTXHJcbi8vIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVxyXG5cclxuZG9jdW1lbnQuYWRkRXZlbnRMaXN0ZW5lcihcIkRPTUNvbnRlbnRMb2FkZWRcIiwgZnVuY3Rpb24oKSB7XHJcbiAgLy8gLi4uXHJcbn0pO1xyXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
